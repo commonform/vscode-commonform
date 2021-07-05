8a28cf6f5e6a2ae636129eb5efd61e0d2f87d49d
Kyle E. Mitchell <kyle@kemitchell.com>
2021-07-04 23:11:20 -0700

    Implement HTML command

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ const numberings = {
 }
 const prepareBlanks = require('commonform-prepare-blanks')
 const signaturePages = require('ooxml-signature-pages')
+const html = require('commonform-html')
 
 const outputChannel = vscode.window.createOutputChannel('Common Form')
 
 exports.activate = context => {
   console.log('Loaded vscode-commonform.')
 
-  const disposable = vscode.commands.registerCommand('vscode-commonform.docx', () => {
+  const docxCommand = vscode.commands.registerCommand('vscode-commonform.docx', () => {
     const document = vscode.window.activeTextEditor.document
     const fileName = path.normalize(document.fileName)
     const dirname = path.dirname(fileName)
@@ -91,7 +92,67 @@ exports.activate = context => {
     }
   })
 
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(docxCommand)
+
+  const htmlCommand = vscode.commands.registerCommand('vscode-commonform.html', () => {
+    const document = vscode.window.activeTextEditor.document
+    const fileName = path.normalize(document.fileName)
+    const dirname = path.dirname(fileName)
+    const extname = path.extname(fileName)
+    const basename = path.basename(fileName, extname)
+    const outputPath = path.join(dirname, `${basename}.html`)
+
+    let parsed
+    try {
+      parsed = commonmark.parse(document.getText())
+    } catch (error) {
+      return fail('Error parsing markup.')
+    }
+
+    const { form, frontMatter, directions } = parsed
+
+    const options = {
+      title: frontMatter.title || 'Untitled Form',
+      edition: frontMatter.edition || undefined,
+      depth: frontMatter.edition || undefined,
+      classNames: frontMatter.classNames || undefined,
+      html5: true,
+      ids: true,
+      lists: true
+    }
+
+    let blanks
+    if (frontMatter.blanks) {
+      if (Array.isArray(frontMatter.blanks)) {
+        blanks = frontMatter.blanks
+      } else {
+        blanks = prepareBlanks(frontMatter.blanks, directions)
+      }
+    } else {
+      blanks = []
+    }
+
+    let output
+    try {
+      output = html(form, blanks, options)
+    } catch (error) {
+      return fail(error.toString())
+    }
+
+    fs.writeFile(outputPath, output, error => {
+      if (error) return fail(error.toString())
+      const message = `Wrote ${outputPath}`
+      console.log(message)
+      outputChannel.append(message + '\n')
+    })
+
+    function fail (message) {
+      vscode.window.showErrorMessage(`stderr: ${message}\n`)
+      outputChannel.append(`stderr: ${message}\n`)
+    }
+  })
+
+  context.subscriptions.push(htmlCommand)
 }
 
 exports.deactivate = () => {}
